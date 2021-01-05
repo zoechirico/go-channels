7cd36adb951472af914abb3c04ab3154cad572d6
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2021-01-05 16:20:35 +0000

    update

--- pkg/day1/day1_test.go
+++ pkg/day1/day1_test.go
@@ -11,5 +11,27 @@ import (
 
 func Test_UsingWaitGroup(t *testing.T) {
 
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
+	go func() {
+
+		defer wg.Done()
+		fmt.Println("We want something to say 1")
+		time.Sleep(1)
+	}()
+
+
+	wg.Add(1)
+
+	go func() {
+
+		defer wg.Done()
+		fmt.Println("We want something to say 2")
+		time.Sleep(1)
+	}()
+
+	wg.Wait()
 
 }
